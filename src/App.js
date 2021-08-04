6c7b8b318c3a1e6f1795c0ec4566e5832562749e
Johnathan Figueroa <johnathan.c.figueroa@gmail.com>
2021-08-03 17:53:13 -0700

    project setup complete

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import axios from 'axios';
+import './App.css';
+
+class App extends React.Component {
+
+
+
+
+  render() {
+    return <h1>My App</h1>;
+  }
+}
+
+export default App;
